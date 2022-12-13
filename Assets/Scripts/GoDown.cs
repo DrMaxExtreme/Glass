5abb3c938d7bd80076963859374a317a739ea84c
DrMax <tehnolog3ooo@yandex.ru>
2022-12-13 17:56:56 +0300

    Alpha_0.3

--- Assets/Scripts/GoDown.cs
+++ Assets/Scripts/GoDown.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class GoDown : MonoBehaviour
 {
-    private float _speed = 0.1f;
+    private float _speed = 0.0001f;
     private Transform _target;
 
     private void Start()
     {
+        _target = transform;
+    }
+
+    private void Update()
+    {
         Ray ray = new Ray(transform.position, -transform.up);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
-            _target.position = hit.point; //в _target.position ничего не сохраняется
-    }
+        if (Physics.Raycast(ray, out hit)) //if hit == null => Vector3.MoveTowards(transform.position, transform.position.y - 1, _speed);
+            _target.position = hit.point;
 
-    void Update()
-    {
-        //transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed);
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed);
 
-        Debug.DrawRay(transform.position, -transform.up * 10, Color.yellow);
-        Debug.DrawRay(transform.position, _target.position * 10, Color.red);
+        Debug.DrawRay(transform.position, _target.position * 12, Color.red);
     }
 }
