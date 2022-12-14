ff6e43aecba48d55a7211263e6e48f3edf892524
DrMax <tehnolog3ooo@yandex.ru>
2022-12-14 11:02:38 +0300

    Alpha_1.0

--- Assets/Scripts/MovementDown.cs
+++ Assets/Scripts/MovementDown.cs
@@ -6,25 +6,33 @@ using UnityEngine;
 public class MovementDown : MonoBehaviour
 {
     private float _speed = 0.1f;
-    private float _distance = 1;
+    private float _distance = 1f;
+    private bool _isMove = false;
+    private Vector3 _targetPosition;
 
     private void Update()
     {
-        Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y - _distance, transform.position.z);
-
         Ray ray = new Ray(transform.position, -transform.up);
 
-        if(Physics.Raycast(ray, _distance))
-            StartCoroutine(MoveDown(targetPosition));
-        else
-            StopCoroutine(MoveDown(targetPosition));
+        Debug.DrawRay(transform.position, -transform.up);
 
+        if(Physics.Raycast(ray, _distance) == false && _isMove == false)
+        {
+            _targetPosition = new Vector3(transform.position.x, transform.position.y - _distance, transform.position.z);
+            _isMove = true;
+            Debug.Log("True");
+        }
+            
+        if(_isMove)
+            MoveDown(_targetPosition);
+
+        if (transform.position == _targetPosition)
+            _isMove = false;
     }
 
-    private IEnumerator MoveDown(Vector3 targetPosition)
+    private void MoveDown(Vector3 targetPosition)
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed);
-
-        yield return null;
+        Debug.Log("Move");
     }
 }
